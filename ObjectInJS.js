d141970fb168df4e3d1b5b5c08b060291e74c531
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-01-15 23:33:09 +0530

    Create ObjectInJS.js

---
+++ ObjectInJS.js
@@ -0,0 +1,15 @@
+//Here the examples of object in JavaScript...
+const profile={
+    username:"Shubh Kulshrestha",
+    posts:1,
+    followers:85,
+    following:157,
+};
+console.log(profile);
+
+const product={
+    name:"Pen",
+    price:200,
+    rating:4,
+};
+console.log(product);
