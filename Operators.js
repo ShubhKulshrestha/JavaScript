b600b157a16c2c7752e6eec5de6583d4fd72111e
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-01-16 22:57:49 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,43 @@
+// Operators in JavaScript...
+// Arithmetic Operator
+let a = 10;
+let b = 5;
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulus
+console.log(a ** b); // Exponentiation
+
+// Unary Operator
+console.log(a++); // Post Increment
+console.log(a);   
+console.log(++a); // Pre Increment
+console.log(a);
+console.log(a--); // Post Decrement
+console.log(a);
+console.log(--a); // Pre Decrement
+console.log(a);
+
+//Assignment Operator
+a += 4; // Addition
+a -= 3; // Subtraction
+a *= 2; // Multiplication
+a /= 5; // Division
+a %= 8; // Modulus
+a **= 1; // Exponentiation
+
+//Comparison Operator
+a == b; // Equal to
+a === b; // Equal to & type
+a != b; // Not equal to
+a !== b; // Not equal & type
+a > b; // Greater than
+a < b; // Less than
+a >= b; // Greater than or equal to
+a <= b; // Less than or equal to
+
+//Logical Operator
+(a<15) && (b>6);//AND 
+(a==6) || (b==6_;//OR
+!(a>b);//NOT
