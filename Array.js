ed5ad7d25213cc4b387ed345e32c7dcfce6d73b6
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-01-21 22:56:30 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,10 @@
+// Arrays in JavaScript...
+let marks=[23,45,67,43,76];
+  console.log(marks);
+  console.log(marks.length);// Arrays length
+
+//Looping over an array...
+let marks=[23,45,67,43,76];
+for(i=0;i<marks.length;i++){
+    console.log(marks[i]);
+  }
