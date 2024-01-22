9db0ca2ec3ae84de965056e0fd1eb223f3cd9193
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-01-22 23:30:38 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -8,3 +8,32 @@ let marks=[23,45,67,43,76];
 for(i=0;i<marks.length;i++){
     console.log(marks[i]);
   }
+// for of loop
+for (i of marks){
+    console.log(i);
+}
+
+// Array Methods...
+//Push()
+ let item=["Apple","Orange","Mango"];
+ item.push("Banana","Grapes");
+ console.log(item);
+
+//pop()
+let items=["Apple","Orange","Mango"];
+ items.pop();
+ console.log(items);
+ let deletedItem=items.pop();
+ console.log(items); 
+ console.log("deleted item is",deletedItem);
+
+ //toString()
+let items=["Apple","Orange","Mango"];
+ console.log(items.toString());
+ console.log(items);
+
+ //concate()
+ let marvelHeroes=["ironman","thor","spiderman"];
+ dc_Heroes=["batman","superman"];
+ let heroes=marvelHeroes.concat(dc_Heroes);
+ console.log(heroes);
