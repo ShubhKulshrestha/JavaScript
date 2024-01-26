74ba8d2a75187a8b8bfb3065b086c36873f81671
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-01-26 23:20:51 +0530

    Create DOM Manipulation.js

---
+++ DOM Manipulation.js
@@ -0,0 +1,13 @@
+//DOM Manipulation in JS...
+// Selecting With id
+document.getElementById("myId");
+
+// Selecting With class 
+document.getElementByClassName("className");
+
+// Selecting With tag
+document.getElementByTagName("p");
+
+// Query Selector
+document.querySelector("myId/.myClass/tag"); // Returns first Elements
+document.querySelectorAll("myId/.myClass/tag"); // Returns a Nodelist
