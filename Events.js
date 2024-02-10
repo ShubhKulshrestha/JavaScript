63afc03958afe2f5c9393f5504a0e8920cfbeb03
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-10 16:43:33 +0530

    Update Events.js

--- Events.js
+++ Events.js
@@ -6,10 +6,12 @@ node.event = () =>{
 
 //Event objects
 Node.event = (e) =>{
-    //handle here
+  console.log(event);
+  console.log(event.type);
+  console.log(event.target);
 }
 
-// event listeners
+//Event listeners
 node.addEventListener(event,callback)
 node.removeEventListener(event,callback)
 //note: the callback reference should be same to remove.
