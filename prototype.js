e28193928fed6e676516b9163993315ba40a7c2b
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-24 21:59:29 +0530

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -1,1 +1,11 @@
-
+// prototype example
+const employee = {
+    calcTax(){
+        console.log("Tax rate is 10%");
+    }
+};
+const Shubh ={
+    salary: 50000,
+};
+Shubh.__proto__ = employee;
+console.log(Shubh.calcTax());
