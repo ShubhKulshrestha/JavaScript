e194a08303ea16495707eab81b488fe8f14fa01c
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-24 22:08:15 +0530

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -9,3 +9,20 @@ const Shubh ={
 };
 Shubh.__proto__ = employee;
 console.log(Shubh.calcTax());
+// Note:-
+//If object and prototype have same method, object's method will be used.
+//Explain with an example..
+const employee = {
+    calcTax(){
+        console.log("Tax rate is 10%");
+    }
+};
+
+const Shubh ={
+    salary: 50000,
+    calcTax(){
+        console.log("Tax rate is 20%");
+    }
+};
+Shubh.__proto__ = employee;
+console.log(Shubh.calcTax());
