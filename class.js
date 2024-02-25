c08f032f3337037cf63f2713981aaa7cb476b0c3
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-25 21:24:25 +0530

    Update class.js

--- class.js
+++ class.js
@@ -1,1 +1,11 @@
-
+//class example...
+class Audicar {
+    start(){
+        console.log("Start");
+    }
+    stop(){
+        console.log("Stop");
+    }
+}
+let fortuner = new Audicar();
+let bmw = new Audicar();
