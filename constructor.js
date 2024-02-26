104d022e445317f8f7b88bafc92ca5e1d9be5b41
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-26 23:29:55 +0530

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,14 @@
+//Constructor Example..
+class Audicar {
+    constructor(){
+            console.log("Working");
+        }
+    start(){
+        console.log("Start");
+    }
+    stop(){
+        console.log("Stop");
+    }
+}
+let fortuner = new Audicar();
+let bmw = new Audicar();
