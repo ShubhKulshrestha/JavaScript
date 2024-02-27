3f0ac407ac97db5934a6a74539300a59c288e150
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-02-27 23:14:17 +0530

    Update Inheritence.js

--- Inheritence.js
+++ Inheritence.js
@@ -6,3 +6,20 @@ class parent{
 }
 class child extends parent{}
 let obj = new child();
+
+class person {
+    eat(){
+        console.log("eat");
+    }
+    sleep(){
+        console.log("sleep");
+    }
+}
+class engeneer extends person{
+    Work(){
+        console.log("Solve Problems, Build Something");
+
+    }
+
+}
+let shubhobj = new engeneer();
