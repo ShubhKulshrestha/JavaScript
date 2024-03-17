222e7fbc17d38eea92b473ac296a566ff025abca
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-03-17 23:09:08 +0530

    Update errorHandling.js

--- errorHandling.js
+++ errorHandling.js
@@ -1,1 +1,14 @@
+// Error handling
+let a = 5;
+let b = 10;
+console.log("a = ",a);
+console.log("b = ",b);
+console.log("a + b = ",a + b);
+try{
+console.log("a + b = ",a + c); // error
+} catch(err){
+    console.error(err);
 
+}
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
