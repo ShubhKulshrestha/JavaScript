0e23708350821a2af6c11a6b9035a07896f9320c
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-04-26 21:10:25 +0530

    Create callback hell.js

---
+++ callback hell.js
@@ -0,0 +1,21 @@
+
+function getData(dataId,getNextdata){
+    //2s
+    setTimeout(()=>{
+        console.log("data",dataId);
+        if(getNextdata){
+        getNextdata();
+        }
+    },2000);         
+}
+//Callback hell
+    getData(1,() =>{
+    console.log("Getting data 2 ...");
+    getData(2,() =>{
+        console.log("Getting data 3 ...");
+        getData(3,() =>{
+            console.log("Getting data 4 ...");
+            getData(4);
+        });
+    });
+});
