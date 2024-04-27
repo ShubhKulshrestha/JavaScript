b643509c915ed91067820d9db0b833145164b7cc
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-04-27 12:45:24 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,1 +1,20 @@
+//Promises
+let promise = new Promise((resolve,reject) =>{
+    console.log("I am a promise");
+});
 
+//pending state
+let promise = new Promise((resolve,reject) =>{
+    console.log("I am a promise");
+    
+});
+//resolve state
+let promise = new Promise((resolve,reject) =>{
+    console.log("I am a promise");
+    resolve("Suceess");
+});
+//reject state
+let promise = new Promise((resolve,reject) =>{
+    console.log("I am a promise");
+    reject("Error");
+});
