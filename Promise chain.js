4c4e143703ab9dbfa16ed899bcfa30a6de544c89
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-06-29 22:36:05 +0530

    Update Promise chain.js

--- Promise chain.js
+++ Promise chain.js
@@ -6,11 +6,21 @@ function asyncFunc(){
        resolve("success");
        },4000);
 });
-
+}
+function asyncFunc2(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() =>{
+       console.log("some data2");
+       resolve("success");
+       },4000);
+});
 }
 console.log("fetching data1");
-let p1 = asyncFunc();
-p1.then((res) =>{
-    console.log(res);
-
+asyncFunc().then((res) =>{
+   console.log("fetching data2");
+    let p2 = asyncFunc2();
+    p2.then((res) =>{
+        
+    });
 });
+  
